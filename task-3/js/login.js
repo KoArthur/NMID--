ddf223abc688d6b6122588cf7343ba8fc820a905
devolopcode <84367253+devolopcode@users.noreply.github.com>
2021-08-10 02:12:13 +0800

    NEW

---
+++ task-3/js/login.js
@@ -0,0 +1,27 @@
+function $$(id){
+    return document.getElementById(id);
+}
+function change(){
+    let Ochange=$$("change");
+    let oVa=$$("validate");
+    if(Ochange.innerText==="◯验证码登陆"){
+        Ochange.innerText="◯密码登陆";
+        oVa.innerHTML='<input type="password" name="" id="" placeholder="请输入验证码">'+
+        '<div id="btn" onclick="getCode()">获取验证码</div>';
+    }else{
+        Ochange.innerText="◯验证码登陆";
+        oVa.innerHTML='<input type="password" name="" id="" placeholder="请输入密码">';
+    }
+}
+
+function forget(){
+    window.location.href="#";
+}
+
+function register(){
+    window.location.href="../register.html";
+}
+
+function getCode(){
+
+}
