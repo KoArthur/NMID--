1276bf79269eb8355d6fc7b02ef0c40fe679d34c
devolopcode <1626104789@qq.com>
2021-08-15 00:05:38 +0800

    new

--- task-3/js/login.js
+++ task-3/js/login.js
@@ -3,17 +3,155 @@ window.onload = function () {
         return document.getElementById(id);
     }
     let oChange = $$("change");
-
+    let oPhone = $$("phone");
+    let oPsw = $$("psw");
+    let oGetCode = document.getElementsByTagName("input")[2];
+    let oLogin = $$("login");
     oChange.onclick = function () {
         if (oChange.innerText === "验证码登陆") {
             oChange.innerText = "密码登陆";
-            let oMode = $$("mode");
-            oMode.innerHTML = '<input type="password" name="psw" class="input" placeholder="请输入验证码">';
-            oMode.innerHTML += '<input type="button" id="getNum" class="btn" value="获取验证码">';
+            oPsw.placeholder = "请输入验证码";
+            oGetCode.id = "getCode";
+            oGetCode.className = "btn";
+            oGetCode.value = "获取验证码";
+            oPsw.type = "text";
+            oPsw.value = "";
+            oPsw.maxLength = "6";
         } else {
             oChange.innerText = "验证码登陆";
-            let oMode = $$("mode");
-            oMode.innerHTML = '<input type="password" name="psw" class="input" placeholder="请输入密码">';
+            oPsw.placeholder = "请输入密码";
+            oGetCode.id = "";
+            oGetCode.className = "clear";
+            oGetCode.value = "";
+            oPsw.type = "password";
+            oPsw.value = "";
+            oPsw.maxLength = "16";
+        }
+    }
+
+
+    oGetCode.onclick = function (e) {
+        let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+        if (oPhone.value == "") {
+            alert("邮箱为空");
+        }
+        if (reg.test(oPhone.value) == false && oPhone.value != "") {
+            alert("邮箱格式不正确");
+        }
+        if (oPhone.value != "" && reg.test(oPhone.value) == true) {
+            let xhr = new XMLHttpRequest();
+            xhr.timeout = 2000;
+            xhr.ontimeout = function () {
+                alert("网络超时");
+            }
+            xhr.onerror = function () {
+                alert("你的网络似乎出了问题");
+            }
+            xhr.responseType = 'json';
+            xhr.open('GET', 'http://47.98.192.167:8800/is_repeat?tel=' + oPhone.value);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        if (Object.values(xhr.response.data).toString() == "true") {
+                            xhr = new XMLHttpRequest();
+                            xhr.timeout = 2000;
+                            xhr.ontimeout = function () {
+                                alert("网络超时");
+                            }
+                            xhr.onerror = function () {
+                                alert("你的网络似乎出了问题");
+                            }
+                            xhr.responseType = 'json';
+                            xhr.open('GET', 'http://47.98.192.167:8800/smsXxs?phone=' + oPhone.value);
+                            xhr.send();
+                            xhr.onreadystatechange = function () {
+                                if (xhr.readyState == 4) {
+                                    if (xhr.status >= 200 && xhr.status < 300) {
+                                        alert("发送成功");
+                                    }
+                                }
+                            }
+                        } else {
+                            alert("邮箱不存在");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    oLogin.onclick = function () {
+        if (oChange.innerText == "验证码登陆") {
+            if (oPhone.value == "") {
+                alert("邮箱不能为空");
+            }
+
+            let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            if (reg.test(oPhone.value) === false && oPhone.value != "") {
+                alert("邮箱格式不对");
+            }
+            if (reg.test(oPhone.value) === true && oPhone.value != "" && oPsw.value != "") {
+                alert("hello wolrd");
+                let xhr = new XMLHttpRequest();
+                xhr.timeout = 2000;
+                xhr.ontimeout = function () {
+                    alert("网络超时");
+                }
+                xhr.onerror = function () {
+                    alert("你的网络似乎出了问题");
+                }
+                xhr.responseType = 'json';
+                xhr.open('POST', 'http://47.98.192.167:8800/users/load');
+                let msg = 'psw=' + oPsw.value + '&tel=' + oPhone.value;
+                xhr.send(msg);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4) {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            alert(xhr.response.data.msg);
+                        }
+                    }
+                }
+            }
+            if (oPsw.value == "") {
+                alert("密码不能为空");
+            }
+        }
+        if (oChange.innerText == "密码登陆") {
+            if (oPhone.value == "") {
+                alert("邮箱不能为空");
+            }
+
+            let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            if (reg.test(oPhone.value) === false && oPhone.value != "") {
+                alert("邮箱格式不对");
+            }
+            if (reg.test(oPhone.value) === true && oPhone.value != "" && oPsw.value != "") {
+                alert("hello wolrd");
+                let xhr = new XMLHttpRequest();
+                xhr.timeout = 2000;
+                xhr.ontimeout = function () {
+                    alert("网络超时");
+                }
+                xhr.onerror = function () {
+                    alert("你的网络似乎出了问题");
+                }
+                xhr.responseType = 'json';
+                xhr.open('POST', 'http://47.98.192.167:8800/users/load');
+                let msg = 'pin=' + oPsw.value + '&tel=' + oPhone.value;
+                xhr.send(msg);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4) {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            alert(xhr.response.data.msg);
+                        }
+                    }
+                }
+            }
+            if (oPsw.value.length < 6) {
+                alert("请输入正确的验证码");
+            }
         }
     }
 
--- task-3/js/register.js
+++ task-3/js/register.js
@@ -8,35 +8,46 @@ window.onload = function () {
     }
     let oName = $$("name");
     oName.onblur = function () {
-        if (oName.value != "") {
-
+        if (oName.value != "" || oName.value.length < 8) {
+            let xhr = new XMLHttpRequest();
+            xhr.timeout = 2000;
+            xhr.ontimeout = function () {
+                alert("网络超时");
+            }
+            xhr.onerror = function () {
+                alert("你的网络似乎出了问题");
+            }
+            xhr.responseType = 'json';
+            xhr.open('GET', 'http://47.98.192.167:8800/is_repeat?nick_name=' + oName.value);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        console.log(Object.values(xhr.response.data).toString());
+                        if (Object.values(xhr.response.data).toString() == 'false') {
+                            document.getElementsByClassName("mark")[0].innerHTML = "✔";
+                        } else {
+                            document.getElementsByClassName("mark")[0].innerHTML = "❌";
+                        }
+                    }
+                }
+            }
         };
     }
     let oPsw = $$("psw");
+    oPsw.onblur = function () {
+        if (oPsw.value != "") {
+            let reg = /^(?=.*[a-z])(?=.*\d)[\s\S]{8,16}$/;
+            if (reg.test(oPsw.value) === true && oPsw.value.length > 7) {
+                document.getElementsByClassName("mark")[1].innerHTML = "✔";
+            } else {
+                document.getElementsByClassName("mark")[1].innerHTML = "❌";
+            }
+        }
+    }
     let oPhone = $$("phone");
     oPhone.onblur = function () {
-        if (oPhone.value != "") {
-            let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
-            if (reg.test(oPhone.value) === true) {
-                let xhr = new XMLHttpRequest();
-            }
-        }
-    }
-    let verifyCode = "";
-    let oCode = $$("code");
-    oCode.onblur = function () {
-        if (oCode.value != "" && verifyCode != "") {
-            if (verifyCode === oCode.value) {
-                console.log("success");
-                document.getElementsByClassName("mark")[3].innerHTML = "✔";
-            } else {
-                console.log("error");
-                document.getElementsByClassName("mark")[3].innerHTML = "❌";
-            }
-        }
-    }
-    let oGetCode = $$("getCode");
-    oGetCode.onclick = function () {
         if (oPhone.value != "") {
             let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
             if (reg.test(oPhone.value) === true) {
@@ -49,7 +60,50 @@ window.onload = function () {
                     alert("你的网络似乎出了问题");
                 }
                 xhr.responseType = 'json';
-                xhr.open('GET', 'http://47.98.192.167:8800/smsXxs?phone=' + oPhone.value);
+                xhr.open('GET', 'http://47.98.192.167:8800/is_repeat?tel=' + oPhone.value);
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                xhr.send();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            console.log(Object.values(xhr.response.data).toString());
+                            if (Object.values(xhr.response.data).toString() == 'false') {
+                                document.getElementsByClassName("mark")[2].innerHTML = "✔";
+                            } else {
+                                document.getElementsByClassName("mark")[2].innerHTML = "❌";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    let verifyCode = "";
+    let oCode = $$("code");
+    oCode.onblur = function () {
+        if (oCode.value != "" && verifyCode != "") {
+            if (verifyCode === oCode.value) {
+                document.getElementsByClassName("mark")[3].innerHTML = "✔";
+            } else {
+                document.getElementsByClassName("mark")[3].innerHTML = "❌";
+            }
+        }
+    }
+    let oGetCode = $$("getCode");
+    oGetCode.onclick = function () {
+        if (oPhone.value != "" && document.getElementsByClassName("mark")[2].innerHTML == "✔") {
+            let reg = /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            if (reg.test(oPhone.value) === true) {
+                let xhr = new XMLHttpRequest();
+                xhr.timeout = 2000;
+                xhr.ontimeout = function () {
+                    alert("网络超时");
+                }
+                xhr.onerror = function () {
+                    alert("你的网络似乎出了问题");
+                }
+                xhr.responseType = 'json';
+                xhr.open('GET', 'http://47.98.192.167:8800/is_repeat?tel=' + oPhone.value);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 xhr.send();
                 xhr.onreadystatechange = function () {
@@ -61,6 +115,29 @@ window.onload = function () {
                 }
             }
         }
+        if (oPhone.value == "") {
+            alert("邮箱为空");
+        } else {
+            alert("邮箱格式不正确");
+        }
     }
-
+    let oRegister = $$("register");
+    oRegister.onclick = function () {
+        if (document.getElementsByClassName("mark")[0].innerHTML == "✔" && document.getElementsByClassName("mark")[1].innerHTML == "✔" && document.getElementsByClassName("mark")[2].innerHTML == "✔" && document.getElementsByClassName("mark")[3].innerHTML == "✔") {
+            let xhr = new XMLHttpRequest();
+            xhr.timeout = 2000;
+            xhr.ontimeout = function () {
+                alert("网络超时");
+            }
+            xhr.onerror = function () {
+                alert("你的网络似乎出了问题");
+            }
+            xhr.responseType = 'json';
+            xhr.open('POST', 'http://47.98.192.167:8800/is_repeat?nick_name=' + oName.value);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send();
+        } else {
+            alert("请审核信息");
+        }
+    }
 }
